cda172ed91a870cb4f7e45615fc243c6714a5cb5
tomotomo <tomotomobile@gmail.com>
2019-02-08 12:55:50 +0900

    dummy data

--- flask_app.py
+++ flask_app.py
@@ -18,7 +18,8 @@ app = Flask(__name__)
 @app.route('/sensor')
 def cpu():
     press, temp = sensor.readData()
-    return jsonify(temperature=round(temp,2), pressure=round(press,2))
+    return jsonify(temperature=1, pressure=1)
+   #  return jsonify(temperature=round(temp,2), pressure=round(press,2))
 
 @app.route('/')
 def home():
